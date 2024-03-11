bfb91e6cee3c90dae98f0b380665991fe2a389a0
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-03-11 16:55:40 +0700

    Adding all models that i needed for this project

---
+++ app/Models/Movie.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $table = 'movies';
+
+    protected $fillable = [
+        'title',
+        'trailer',
+        'movie',
+        'casts',
+        'categories',
+        'small_thumbnail',
+        'large_thumbnail',
+        'about',
+        'short_about',
+        'duration',
+        'featured',
+    ];
+}
