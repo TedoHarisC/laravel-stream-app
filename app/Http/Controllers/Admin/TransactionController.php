691fcdc0878076ccab105f1d435b351e40c3a742
Tedo Haris Chandra <tedoharischandra29@gmail.com>
2024-03-30 08:24:55 +0700

    Menambahkan data transaction dan relasi nya

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
     public function index()
     {
-        return view('admin.transactions');
+        $transactions = Transaction::with([
+            'package',
+            'user'
+        ])->get();
+        //dd($transactions);
+        return view('admin.transactions', ['transactions' => $transactions]);
     }
 }
